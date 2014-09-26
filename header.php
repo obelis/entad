4b1d8843865a131c6fc8bdc22eb17d1347833e50
Kevin McMahon <kevinmcmahon@obelismedia.com>
2014-09-26 12:25:08 -0400

    Header Styling
    Possible custom style conflicting with bootstrap, placed id on separate
    div

--- header.php
+++ header.php
@@ -95,14 +95,16 @@
             <div class="col-sm-4" id="header-tagline">
                 <p class="animated fadeInLeft"><?php bloginfo('description'); ?></p>
             </div>
-            <div class="col-sm-4" id="header-locations">
-                <h3>3 Locations To Serve You</h2>
-                <ul>
-                    <li><?php echo $main_street . ", " . $main_city; ?><a href="<?php echo $main_g; ?>" rel="publisher" class="social-icon soc-google-plus"><span class="description">Google+ Main Location</span><span class="fa fa-google-plus"></span></a></li>
-                    <li><?php echo $second_street . ", " . $second_city; ?><a href="<?php echo $second_g; ?>" rel="publisher" class="social-icon soc-google-plus"><span class="description">Google+ Second Location</span><span class="fa fa-google-plus"></span></a></li>
-                    <li><?php echo $third_street . ", " . $third_city; ?><a href="<?php echo $third_g; ?>" rel="publisher" class="social-icon soc-google-plus"><span class="description">Google+ Third Location</span><span class="fa fa-google-plus"></span></a></li>
-                </ul>
-                <a href="/walk-hearing-aid-hours/" id="header-walkin">For Walk-In Hours Click Here</a>
+            <div class="col-sm-4">
+                <div id="header-locations">
+                    <h3>3 Locations To Serve You</h2>
+                    <ul>
+                        <li><?php echo $main_street . ", " . $main_city; ?><a href="<?php echo $main_g; ?>" rel="publisher" class="social-icon soc-google-plus"><span class="description">Google+ Main Location</span><span class="fa fa-google-plus"></span></a></li>
+                        <li><?php echo $second_street . ", " . $second_city; ?><a href="<?php echo $second_g; ?>" rel="publisher" class="social-icon soc-google-plus"><span class="description">Google+ Second Location</span><span class="fa fa-google-plus"></span></a></li>
+                        <li><?php echo $third_street . ", " . $third_city; ?><a href="<?php echo $third_g; ?>" rel="publisher" class="social-icon soc-google-plus"><span class="description">Google+ Third Location</span><span class="fa fa-google-plus"></span></a></li>
+                    </ul>
+                    <a href="/walk-hearing-aid-hours/" id="header-walkin">For Walk-In Hours Click Here</a>
+                </div>
             </div>
         </div>
         
